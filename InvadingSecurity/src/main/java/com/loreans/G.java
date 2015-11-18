e8031edadb1b17538013bfa60de588baf3ac19f2
nikesh <niksw7@gmail.com>
2015-11-18 06:21:11 +0530

    Resetting the checked in structure

--- InvadingSecurity/src/main/java/com/loreans/G.java
+++
@@ -1,9 +0,0 @@
-package com.loreans;
-
-class G {
-    public static void main(String[] args) {
-        final int i = (int)127;
-        byte b = i;
-        System.out.println(i + ", " + b);
-    }
-}
